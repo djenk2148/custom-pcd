be1d43f5ad45bfd77208e563a3a08477bb7fbe23
djenk2148 <dillon.jenkins0@outlook.com>
2026-07-03 13:32:39 -0400

    Delete ops/2.regex.sql

--- ops/2.regex.sql
+++
@@ -1,6 +0,0 @@
--- ============================================================================
--- REGULAR EXPRESSIONS - General
--- ============================================================================
-
-INSERT INTO regular_expressions (name, pattern, regex101_id, description) VALUES ('_nzb', '_nzb\b', NULL, NULL);
-
