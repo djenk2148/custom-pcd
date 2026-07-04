dccc5b05da17434bc54c4fb2d5e281cee8ed0dde
djenk2148 <dillon.jenkins0@outlook.com>
2026-07-04 17:17:35 -0400

    Delete ops/7.custom-format-conditions.sql

--- ops/7.custom-format-conditions.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO custom_format_conditions (custom_format_name, name, type, arr_type, negate, required) VALUES ('Audio Channel: 1.0', 'Audio Channel: 1.0', 'release_title', 'radarr', 0, 0);
-INSERT INTO custom_format_conditions (custom_format_name, name, type, arr_type, negate, required) VALUES ('Audio Channel: 1.0', 'Audio Channel: 1.0', 'release_title', 'sonarr', 0, 0);
