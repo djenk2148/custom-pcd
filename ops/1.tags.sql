a0d94f29080dd90a906d585fb2e84ba4e59332c0
djenk2148 <dillon.jenkins0@outlook.com>
2026-07-04 17:41:40 -0400

    Update 1.tags.sql

--- ops/1.tags.sql
+++ ops/1.tags.sql
@@ -1,1 +1,7 @@
-
+-- ============================================================================
+-- TAGS
+-- ============================================================================
+INSERT INTO tags (name) VALUES ('Anime');
+INSERT INTO tags (name) VALUES ('Audio Codec');
+INSERT INTO tags (name) VALUES ('Optional');
+INSERT INTO tags (name) VALUES ('Video Codec');
