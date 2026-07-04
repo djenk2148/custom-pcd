14d9e47dcd002c4c9f3df75d884115086c468d2a
djenk2148 <dillon.jenkins0@outlook.com>
2026-07-03 21:42:39 -0400

    Update 4.custom-formats.sql

--- ops/4.custom-formats.sql
+++ ops/4.custom-formats.sql
@@ -6,7 +6,40 @@ INSERT INTO custom_formats (name, description, include_in_rename) VALUES ('Acces
 INSERT INTO custom_formats (name, description, include_in_rename) VALUES ('Accessibility: BASL', 'BASL is a unique variety of ASL that developed primarily due to the racial segregation of schools in the American South during the 19th and 20th centuries.', 0);
 INSERT INTO custom_formats (name, description, include_in_rename) VALUES ('Accessibility: BSL', 'British Sign Language, a visual-gestural language used primarily by the UK Deaf community. It has its own unique grammar and syntax, completely distinct from spoken English. It became legally recognized in England, Wales, and Scotland under the BSL Act 2022.', 0);
 
+
+-- ============================================================================
+-- AUDIO CHANNEL
+-- ============================================================================
+
+-- ============================================================================
+-- COLOR DEPTH
+-- ============================================================================
+
+-- ============================================================================
+-- FILE FORMAT
+-- ============================================================================
+
+
+-- ============================================================================
+-- RESOLUTIONS
+-- ============================================================================
+INSERT INTO custom_formats (name, description, include_in_rename) VALUES ('Resolution: 360p', '', 0);
+INSERT INTO custom_formats (name, description, include_in_rename) VALUES ('Resolution: 480p', '', 0);
+INSERT INTO custom_formats (name, description, include_in_rename) VALUES ('Resolution: 560p', '', 0);
+INSERT INTO custom_formats (name, description, include_in_rename) VALUES ('Resolution: 576p', '', 0);
+INSERT INTO custom_formats (name, description, include_in_rename) VALUES ('Resolution: 720p', '', 0);
+INSERT INTO custom_formats (name, description, include_in_rename) VALUES ('Resolution: 1080p', '', 0);
+INSERT INTO custom_formats (name, description, include_in_rename) VALUES ('Resolution: 1440p', '', 0);
+INSERT INTO custom_formats (name, description, include_in_rename) VALUES ('Resolution: 2160p', '', 0);
+
+
 -- ============================================================================
 -- VIDEO CODECS
 -- ============================================================================
-INSERT INTO custom_formats (name, description, include_in_rename) VALUES ('Video Codec: AV1', 'test', 0);
+INSERT INTO custom_formats (name, description, include_in_rename) VALUES ('Video Codec: 264', 'H.264, also known as Advanced Video Coding (AVC), is the most widely used video compression standard globally. It shrinks large, uncompressed video files into manageable sizes while preserving high picture quality, making it the universal standard for streaming video, Blu-ray discs, and broadcasting.', 0);
+INSERT INTO custom_formats (name, description, include_in_rename) VALUES ('Video Codec: 265', 'H.265, officially known as High Efficiency Video Coding (HEVC), is a highly advanced video compression standard designed to compress video files to about half the size of older formats (like H.264) without losing visual quality. It is the foundational technology that makes streaming 4K and 8K content feasible.', 0);
+INSERT INTO custom_formats (name, description, include_in_rename) VALUES ('Video Codec: 266', 'H.266, also known as Versatile Video Coding (VVC), is a next-generation video compression standard finalized in 2020. It is the official successor to H.265 (HEVC), designed to reduce video bandwidth and file sizes by 40–50% for equivalent perceptual quality. It is optimized for 4K, 8K, and immersive VR video.', 0);
+INSERT INTO custom_formats (name, description, include_in_rename) VALUES ('Video Codec: AV1', 'AOMedia Video 1 (AV1) is an open, royalty-free video coding format designed for highly efficient video streaming over the internet. Developed by the Alliance for Open Media (AOMedia)—a massive tech consortium including Google, Apple, Microsoft, Netflix, and Amazon—it serves as the license-free successor to VP9 and a direct competitor to H.265 (HEVC).', 0);
+INSERT INTO custom_formats (name, description, include_in_rename) VALUES ('Video Codec: MPEG2', 'MPEG-2 (also known as H.262) is a standard lossy video and audio compression format developed in the 1990s. It is widely recognized as the industry standard for DVD-Video discs and traditional terrestrial, cable, and satellite television broadcasts, renowned for maintaining sharp, high-quality visuals.', 0);
+INSERT INTO custom_formats (name, description, include_in_rename) VALUES ('Video Codec: VP8', 'VP8 is an open-source, royalty-free video compression format developed by On2 Technologies and released by Google. Primarily used alongside the WebM Project', 0);
+INSERT INTO custom_formats (name, description, include_in_rename) VALUES ('Video Codec: VP9', 'VP9 is a royalty-free, open-source video codec developed by Google to compress and stream high-quality video efficiently. It significantly reduces bandwidth requirements—often needing up to 50% less bitrate than older formats like H.264/AVC for the same visual quality.', 0);
